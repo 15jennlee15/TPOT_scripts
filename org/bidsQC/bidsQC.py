bb2cb0c783820ed571ea0386ecbfefd3316b2fed
Lauren Kahn <laurenkahn@PSY-C4DMH-STB495.local>
2019-03-13 16:44:36 -0700

    Fix for/if nesting

--- org/bidsQC/bidsQC.py
+++ org/bidsQC/bidsQC.py
@@ -99,9 +99,9 @@ def append_series_number(sequence_fullpath:str, bidsdir:str, tasks_to_order: lis
     """
     write_to_outputlog('Appending sequence numbers')
     sequence_files = os.listdir(sequence_fullpath)
-    files_all_target_tasks = [sequence_file for sequence_file in sequence_files for task in tasks_to_order if str(task) in sequence_file]
+    # files_all_target_tasks = [sequence_file for sequence_file in sequence_files for task in tasks_to_order if str(task) in sequence_file]
+    files_all_target_tasks = [sequence_file for sequence_file in sequence_files if any(str(task) in sequence_file for task in tasks_to_order)]
     print(files_all_target_tasks)
-    print(tasks_to_order)
     extensions = '.nii.gz', '.json'
     json_files = [f for f in files_all_target_tasks if f.endswith('.json')]
     print(json_files)
