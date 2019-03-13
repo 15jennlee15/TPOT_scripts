61b8c2d1b1d7320a33a9038f6f2ef3d5f20aea66
Lauren Kahn <laurenkahn@PSY-C4DMH-STB495.local>
2019-03-13 15:34:20 -0700

    Edit config bidsQC file so that affect gets runnum appended

--- org/bidsQC/config_bidsQC.py
+++ org/bidsQC/config_bidsQC.py
@@ -37,8 +37,8 @@ gzipped = True
 # Do we want runs based on sequence order? Yes = True
 # This is for tasks that use counterbalancing (of e.g. stimulus blocks) and for which we want to identify the content
 # with the 'acq-' label and order administered with the 'task-' label.
-order_sequences = False
-tasks_to_order = 'task1', 'task2'
+order_sequences = True
+tasks_to_order = 'affect'
 
 
 # Paths that are required and that should not be changed
