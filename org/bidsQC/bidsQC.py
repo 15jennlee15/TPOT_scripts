2cdaf59be5214a27926e66191b6100e2ff910469
Lauren Kahn <laurenkahn@PSY-C4DMH-STB495.local>
2019-03-13 15:12:11 -0700

    Edit study_config for bidsQC redo

--- org/bidsQC/bidsQC.py
+++ org/bidsQC/bidsQC.py
@@ -337,15 +337,12 @@ def fix_files(sequence_fullpath: str, file_group: str, expected_numfiles: int, e
         return
     if len(found_files) > expected_numfiles:
         difference = len(found_files) - expected_numfiles
-        print(difference)
-        print(found_files)
         found_files.sort()
         write_to_outputlog("\n FIXING FILES: %s \n" % (extension))
         for found_file in found_files:
             try:
                 run_index = found_file.index("_run-")
                 run_number = found_file[run_index + 5:run_index + 7]
-                print(run_number)
                 run_int = int(run_number) 
                 targetfile_fullpath = os.path.join(sequence_fullpath, found_file)
                 if run_int <= difference: 
@@ -359,7 +356,7 @@ def fix_files(sequence_fullpath: str, file_group: str, expected_numfiles: int, e
                         int_str = str(new_int)
                         new_runnum = int_str.zfill(2)
                         new_runstr = 'run-' + new_runnum
-                        os.rename(targetfile_fullpath, targetfile_fullpath.replace(found_file[run_index + 2:run_index + 7], new_runstr))
+                        os.rename(targetfile_fullpath, targetfile_fullpath.replace(found_file[run_index + 1:run_index + 7], new_runstr))
                         write_to_outputlog("RENAMED: %s with run-%s" % (targetfile_fullpath, new_runnum))
             except ValueError:
                 write_to_errorlog('VALUE ERROR in fix_files:\n    Subject: %s\n     File: %s' %(subject, found_file))
