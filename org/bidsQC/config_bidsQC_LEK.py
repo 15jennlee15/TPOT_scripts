f9a4cf1ebb1e47d726da88253b892c1cab9746f9
Lauren Kahn <laurenkahn@PSY-C4DMH-STB495.local>
2019-04-24 15:52:47 -0700

    Create LEK versions of bidsQC + config file that tracks filecounts in a matrix

---
+++ org/bidsQC/config_bidsQC_LEK.py
@@ -0,0 +1,54 @@
+import os
+from datetime import datetime
+from bidsQC_classes import Sequence
+from bidsQC_classes import TimePoint
+
+######################## CONFIGURAGBLE PART BELOW ########################
+
+# Set study info (change these for your study)
+# These variables are used only in this file for path names.
+# They can be removed if desired.
+group = 'adapt_lab'
+study = 'TPOT'
+
+# Set directories (Check these for your study)
+# These variables are used in the main script and need to be defined here. 
+# They need to exist prior to running the script.
+logdir = os.path.join(os.getcwd(), 'logs_bidsQC')  # Where log files will go
+# bidsdir = os.path.join(os.sep, 'projects', group, 'shared', study, 'bids_data')  # Where your subjects' nifti directories are
+bidsdir = os.path.join(os.sep, '/Users/laurenkahn/Desktop', study, 'bids_data')  # Where your subjects' nifti directories are
+
+
+# Create a dictionary (the thing below) for each timepoint in your study where the pairs are 'sequence_directory_name' : 'expected_number_runs'
+# Each unique version of a sequence gets its own entry, e.g. 'gng_acq-1' and 'gng_acq-2'
+
+# alphabetized to be consistent with searching thru folders; should make more flexible ***
+sequence1 = Sequence('anat', {'T1w':1})
+sequence2 = Sequence('dwi', {'acq-lr':1, 'acq-rl':1})
+sequence3 = Sequence('fmap', {'dir-ap':1, 'dir-pa':1})
+sequence4 = Sequence('func', {'affect': 2, 'rest': 2, 'video': 1})
+
+timepoint1 = TimePoint('ses-1', [sequence1, sequence2, sequence3, sequence4])
+expected_timepoints = [timepoint1]
+
+# Files g-zipped or not? 
+# NOTE: All files must be either zipped or unzipped. 
+# A mixture won't work properly.
+gzipped = True
+
+
+# Do we want runs based on sequence order? Yes = True
+# This is for tasks that use counterbalancing (of e.g. stimulus blocks) and for which we want to identify the content
+# with the 'acq-' label and order administered with the 'task-' label.
+order_sequences = True
+tasks_to_order = 'affect', #comma makes it a tuple instead of a string
+
+
+# Paths that are required and that should not be changed
+tempdir = os.path.join(bidsdir, 'tmp_dcm2bids')  # holding folder for undesired files
+outputlog = os.path.join(logdir, 'outputlog_bidsQC' + datetime.now().strftime('%Y%m%d-%H%M%S') + '.txt')
+errorlog = os.path.join(logdir, 'errorlog_bidsQC' + datetime.now().strftime('%Y%m%d-%H%M%S') + '.txt')
+outputmat_json = os.path.join(logdir, 'outputmat_json_bidsQC' + datetime.now().strftime('%Y%m%d-%H%M%S') + '.txt')
+outputmat_nii = os.path.join(logdir, 'outputmat_nii_bidsQC' + datetime.now().strftime('%Y%m%d-%H%M%S') + '.txt')
+runmat = os.path.join(logdir, 'runmat_bidsQC' + datetime.now().strftime('%Y%m%d-%H%M%S') + '.txt')
+derivatives = os.path.join(bidsdir, 'derivatives')  # Where processed data will go
