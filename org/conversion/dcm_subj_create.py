e901a375ad66dc1a56a8743554d3c2335d30a9eb
pettitta <adampettitt89@gmail.com>
2019-06-20 16:50:58 -0700

    mc

--- org/conversion/dcm_subj_create.py
+++ org/conversion/dcm_subj_create.py
@@ -21,8 +21,13 @@ subjectdir_contents = os.listdir(dicomdir)
 subjectdir_contents.sort()
 
 # filter subject directory for anything less than 16 characters (this is study specific)
-subjectdir_contents = list(filter(lambda k: len(k) < 16 or lambda k: len(k) == 24, subjectdir_contents))
+subjectdir_contents = list(filter(lambda k: [len(k) < 16 or len(k) == 24], subjectdir_contents))
 
+test = [directory for directory in subjectdir_contents if any(sub in directory for sub in exclusion)]
+
+testes = list(set(subjectdir_contents)-set(test))
+
+subjectdir_contents = list(filter(lambda k: ['physio' or 'hantom' or 'Blogs' or 'lcni' or 'ppp' or 'pumpkin' or 'trigger' not in k], subjectdir_contents))
 
 
 # filters subject directory for anything with 'SH' in the name (this is study specific)
