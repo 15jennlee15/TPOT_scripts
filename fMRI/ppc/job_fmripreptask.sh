abc107e7817c22bc6ea54b5ceb6b8c5b0cc5fe25
Adam Pettitt <adampettitt89@gmail.com>
2019-08-05 11:53:34 -0700

    mc

--- fMRI/ppc/job_fmripreptask.sh
+++ fMRI/ppc/job_fmripreptask.sh
@@ -31,7 +31,7 @@ echo -e "\n"
 
 export FS_LICENSE=/projects/adapt_lab/shared/TPOT/TPOT_Scripts/rsfMRI/license.txt
 
-FMRIPREP_OPTS="--output-spaces T1w MNI152NLin2009cAsym fsaverage5 fsnative --cifti-output --use-aroma --write-graph"
+FMRIPREP_OPTS="--output-spaces T1w MNI152NLin2009cAsym fsaverage5 fsnative --cifti-output --write-graph"
 
 singularity run --bind "${group_dir}":"${group_dir}" ${image} ${bids_dir} ${derivatives} participant \
 --participant-label $subid \
