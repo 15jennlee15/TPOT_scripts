9b247f855251891040686ddc1166e6e01442ed29
pettitta <adampettitt89@gmail.com>
2019-09-14 11:00:11 -0500

    mc

--- dMRI/preproc/batch_preproc_diff.sh
+++ dMRI/preproc/batch_preproc_diff.sh
@@ -13,6 +13,6 @@ SUBJLIST=`cat subject_list.txt`
 #SUBJLIST=`cat alignment.txt`
 
 for SUBID in $SUBJLIST
- do sbatch --export=all,subid=${SUBID} --job-name diffpreproc_"${SUBID}" --partition=short --mem-per-cpu=3G --time=5:00:00 --nodes=1 --cpus-per-task=1 -o -A adapt_lab "${STUDY}"/TPOT_Scripts/dMRI/preproc/output/"${SUBID}"_diffpreproc_output.txt -e "${STUDY}"/TPOT_Scripts/dMRI/preproc/output/"${SUBID}"_diffpreproc_error.txt preproc_diff.sh
+ do sbatch --export=all,subid=${SUBID} --job-name diffpreproc_"${SUBID}" --partition=short --mem-per-cpu=3G --time=5:00:00 --nodes=1 --cpus-per-task=1 -A adapt_lab -o "${STUDY}"/TPOT_Scripts/dMRI/preproc/output/"${SUBID}"_diffpreproc_output.txt -e "${STUDY}"/TPOT_Scripts/dMRI/preproc/output/"${SUBID}"_diffpreproc_error.txt preproc_diff.sh
 done
 
