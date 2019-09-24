440cf5522b0dbd66c731add7483ba357e33f6e34
pettitta <adampettitt89@gmail.com>
2019-09-24 09:36:33 -0700

    mc

--- fMRI/ppc/smooth/smooth.m
+++ fMRI/ppc/smooth/smooth.m
@@ -4,7 +4,7 @@
 % cfg_basicio BasicIO - Unknown
 %-----------------------------------------------------------------------
 matlabbatch{1}.cfg_basicio.file_dir.file_ops.file_fplist.dir = {'/projects/adapt_lab/shared/TPOT/bids_data/derivatives/fmriprep/sub-TPOT002/ses-1/func'};
-matlabbatch{1}.cfg_basicio.file_dir.file_ops.file_fplist.filter = '*MNI152NLin2009cAsym_desc-preproc.nii.gz';
+matlabbatch{1}.cfg_basicio.file_dir.file_ops.file_fplist.filter = '*MNI152NLin2009cAsym_desc-preproc_bold.nii.gz';
 matlabbatch{1}.cfg_basicio.file_dir.file_ops.file_fplist.rec = 'FPListRec';
 matlabbatch{2}.cfg_basicio.file_dir.file_ops.cfg_gunzip_files.files(1) = cfg_dep('File Selector (Batch Mode): Selected Files (*MNI152NLin2009cAsym_preproc.nii.gz)', substruct('.','val', '{}',{1}, '.','val', '{}',{1}, '.','val', '{}',{1}, '.','val', '{}',{1}), substruct('.','files'));
 matlabbatch{2}.cfg_basicio.file_dir.file_ops.cfg_gunzip_files.outdir = {''};
