ed6e840b1a6b2d38d924a8f80089a2fddee117bb
Jennifer Lewis <15jennlee15@gmail.com>
2019-11-07 11:43:30 -0800

    update time

--- fMRI/ppc/batch_fmripreptask.sh
+++ fMRI/ppc/batch_fmripreptask.sh
@@ -25,6 +25,6 @@ for SUBJ in $SUBJLIST; do
 #SUBID=`echo $SUBJ|awk '{print $1}' FS=","`
 #SESSID=`echo $SUBJ|awk '{print $2}' FS=","`
 	
-sbatch --export subid=${SUBJ},group_dir=${group_dir},study=${study},container=${container},task=${task} --job-name fmriprep_"${SUBJ}" --partition=long -A "${group}" --cpus-per-task=16 --mem-per-cpu=75G —time=20:00:00 -o "${group_dir}"/"${study}"/TPOT_Scripts/fMRI/ppc/output/"${task}"_output/"${SUBJ}"_fmriprep_output.txt -e "${group_dir}"/"${study}"/TPOT_Scripts/fMRI/ppc/output/"${task}"_output/"${SUBJ}"_fmriprep_error.txt job_fmripreptask.sh
+sbatch --export subid=${SUBJ},group_dir=${group_dir},study=${study},container=${container},task=${task} --job-name fmriprep_"${SUBJ}" --partition=long -A "${group}" --cpus-per-task=16 --mem-per-cpu=75G —-time=2-20:00:00 -o "${group_dir}"/"${study}"/TPOT_Scripts/fMRI/ppc/output/"${task}"_output/"${SUBJ}"_fmriprep_output.txt -e "${group_dir}"/"${study}"/TPOT_Scripts/fMRI/ppc/output/"${task}"_output/"${SUBJ}"_fmriprep_error.txt job_fmripreptask.sh
 	
 done
