980b372fda540dce0e7793e09361f199c7689fca
Jennifer Lewis <15jennlee15@gmail.com>
2020-01-05 11:18:40 -0800

    sudo

--- fMRI/ppc/auto-motion-fmriprep/install_packages.bash
+++ fMRI/ppc/auto-motion-fmriprep/install_packages.bash
@@ -7,6 +7,6 @@
 #SBATCH --mem-per-cpu=1000
 #SBATCH --partition=short,fat,long,longfat
 
-module load R gcc
+module load sudo R gcc
 
 Rscript --verbose -e "osuRepo = 'http://ftp.osuosl.org/pub/cran/'; if(!require(tidyverse)){ install.packages('tidyverse',repos=osuRepo) }; if(!require(caret)){ install.packages('caret',repos=osuRepo) }; message('Installation complete')"
