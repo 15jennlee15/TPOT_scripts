1a00e7006a3ec892d04cb5e3f4a8866a63c99e32
Jennifer Lewis <15jennlee15@gmail.com>
2020-02-07 13:33:43 -0800

    reverted back

--- fMRI/fx/models/batch_spm_job_label_fx_models.sh
+++ fMRI/fx/models/batch_spm_job_label_fx_models.sh
@@ -44,8 +44,7 @@ if [ ! -d "${STUDY}/TPOT_Scripts/fMRI/fx/models/affect/sid_batches" ]; then
     mkdir -v "${STUDY}/TPOT_Scripts/fMRI/fx/models/affect/sid_batches"
 fi
 
-if [ ! -d "${STUDY}/TPOT_Scripts/fMRI/fx/models/affect/sid_batches/matlabbatch_job_label” ]
-    then
+if [ ! -d "${STUDY}/TPOT_Scripts/fMRI/fx/models/affect/sid_batches/matlabbatch_job_label” ]; then
     mkdir -v "${STUDY}/TPOT_Scripts/fMRI/fx/models/affect/sid_batches/matlabbatch_job_label”
 fi
 
