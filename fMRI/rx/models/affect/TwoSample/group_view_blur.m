7edb9a49698017c91c0e3fd2d133653428b4d26f
Jennifer Lewis <15jennlee15@gmail.com>
2020-05-11 11:00:29 -0700

    fixed spelling error

--- fMRI/rx/models/affect/TwoSample/group_view_blur.m
+++ fMRI/rx/models/affect/TwoSample/group_view_blur.m
@@ -3,7 +3,7 @@
 % spm SPM - SPM12 (7219)
 % cfg_basicio BasicIO - Unknown
 %-----------------------------------------------------------------------
-matlabbatch{1}.spm.stats.factorial_design.dir = {'projects/adapt_lab/shared/TPOT/out/rx/models/affect/TwoSample/View_Blur_groups'};
+matlabbatch{1}.spm.stats.factorial_design.dir = {'projects/adapt_lab/shared/TPOT/out/rx/models/affect/TwoSample/group_view_blur'};
 %%
 matlabbatch{1}.spm.stats.factorial_design.des.t2.scans1 = {
                                                          'projects/adapt_lab/shared/TPOT/out/fx/models/affect/sub-TPOT002/con_0001.nii'
@@ -83,7 +83,8 @@ matlabbatch{1}.spm.stats.factorial_design.des.t2.scans1 = {
 'projects/adapt_lab/shared/TPOT/out/fx/models/affect/sub-TPOT166/con_0001.nii'
                                                           };
 %%
-matlabbatch{1}.spm.stats.factorial_design.des.t2.scans2 = {'/projects/adapt_lab/shared/TPOT/out/fx/models/affect/sub-TPOT005/con_0001.nii'
+matlabbatch{1}.spm.stats.factorial_design.des.t2.scans2 = {
+    '/projects/adapt_lab/shared/TPOT/out/fx/models/affect/sub-TPOT005/con_0001.nii'
 '/projects/adapt_lab/shared/TPOT/out/fx/models/affect/sub-TPOT008/con_0001.nii'
 '/projects/adapt_lab/shared/TPOT/out/fx/models/affect/sub-TPOT020/con_0001.nii'
 '/projects/adapt_lab/shared/TPOT/out/fx/models/affect/sub-TPOT035/con_0001.nii'
