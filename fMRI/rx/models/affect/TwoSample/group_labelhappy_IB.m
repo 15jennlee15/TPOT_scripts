da25ac063257e668b4b3464b0f16df076e89ff69
Jennifer Lewis <15jennlee15@gmail.com>
2020-05-11 13:39:22 -0700

    create all the group scripts

---
+++ fMRI/rx/models/affect/TwoSample/group_labelhappy_IB.m
@@ -0,0 +1,176 @@
+ %-----------------------------------------------------------------------
+% Job saved on 22-Jan-2019 17:01:56 by cfg_util (rev $Rev: 6942 $)
+% spm SPM - SPM12 (7219)
+% cfg_basicio BasicIO - Unknown
+%-----------------------------------------------------------------------
+matlabbatch{1}.spm.stats.factorial_design.dir = {'/projects/adapt_lab/shared/TPOT/out/rx/models/affect/TwoSample/group_labelhappy_IB'};
+%%
+matlabbatch{1}.spm.stats.factorial_design.des.t2.scans1 = {
+'/projects/adapt_lab/shared/TPOT/out/fx/models/affect/sub-TPOT002/con_0022.nii'
+'/projects/adapt_lab/shared/TPOT/out/fx/models/affect/sub-TPOT004/con_0022.nii'
+'/projects/adapt_lab/shared/TPOT/out/fx/models/affect/sub-TPOT006/con_0022.nii'
+'/projects/adapt_lab/shared/TPOT/out/fx/models/affect/sub-TPOT007/con_0022.nii'
+'/projects/adapt_lab/shared/TPOT/out/fx/models/affect/sub-TPOT009/con_0022.nii'
+'/projects/adapt_lab/shared/TPOT/out/fx/models/affect/sub-TPOT010/con_0022.nii'
+'/projects/adapt_lab/shared/TPOT/out/fx/models/affect/sub-TPOT011/con_0022.nii'
+'/projects/adapt_lab/shared/TPOT/out/fx/models/affect/sub-TPOT013/con_0022.nii'
+'/projects/adapt_lab/shared/TPOT/out/fx/models/affect/sub-TPOT014/con_0022.nii'
+'/projects/adapt_lab/shared/TPOT/out/fx/models/affect/sub-TPOT015/con_0022.nii'
+'/projects/adapt_lab/shared/TPOT/out/fx/models/affect/sub-TPOT017/con_0022.nii'
+'/projects/adapt_lab/shared/TPOT/out/fx/models/affect/sub-TPOT019/con_0022.nii'
+'/projects/adapt_lab/shared/TPOT/out/fx/models/affect/sub-TPOT021/con_0022.nii'
+'/projects/adapt_lab/shared/TPOT/out/fx/models/affect/sub-TPOT023/con_0022.nii'
+'/projects/adapt_lab/shared/TPOT/out/fx/models/affect/sub-TPOT024/con_0022.nii'
+'/projects/adapt_lab/shared/TPOT/out/fx/models/affect/sub-TPOT025/con_0022.nii'
+'/projects/adapt_lab/shared/TPOT/out/fx/models/affect/sub-TPOT026/con_0022.nii'
+'/projects/adapt_lab/shared/TPOT/out/fx/models/affect/sub-TPOT027/con_0022.nii'
+'/projects/adapt_lab/shared/TPOT/out/fx/models/affect/sub-TPOT028/con_0022.nii'
+'/projects/adapt_lab/shared/TPOT/out/fx/models/affect/sub-TPOT029/con_0022.nii'
+'/projects/adapt_lab/shared/TPOT/out/fx/models/affect/sub-TPOT030/con_0022.nii'
+'/projects/adapt_lab/shared/TPOT/out/fx/models/affect/sub-TPOT032/con_0022.nii'
+'/projects/adapt_lab/shared/TPOT/out/fx/models/affect/sub-TPOT033/con_0022.nii'
+'/projects/adapt_lab/shared/TPOT/out/fx/models/affect/sub-TPOT034/con_0022.nii'
+'/projects/adapt_lab/shared/TPOT/out/fx/models/affect/sub-TPOT036/con_0022.nii'
+'/projects/adapt_lab/shared/TPOT/out/fx/models/affect/sub-TPOT037/con_0022.nii'
+'/projects/adapt_lab/shared/TPOT/out/fx/models/affect/sub-TPOT038/con_0022.nii'
+'/projects/adapt_lab/shared/TPOT/out/fx/models/affect/sub-TPOT039/con_0022.nii'
+'/projects/adapt_lab/shared/TPOT/out/fx/models/affect/sub-TPOT040/con_0022.nii'
+'/projects/adapt_lab/shared/TPOT/out/fx/models/affect/sub-TPOT041/con_0022.nii'
+'/projects/adapt_lab/shared/TPOT/out/fx/models/affect/sub-TPOT042/con_0022.nii'
+'/projects/adapt_lab/shared/TPOT/out/fx/models/affect/sub-TPOT043/con_0022.nii'
+'/projects/adapt_lab/shared/TPOT/out/fx/models/affect/sub-TPOT044/con_0022.nii'
+'/projects/adapt_lab/shared/TPOT/out/fx/models/affect/sub-TPOT045/con_0022.nii'
+'/projects/adapt_lab/shared/TPOT/out/fx/models/affect/sub-TPOT048/con_0022.nii'
+'/projects/adapt_lab/shared/TPOT/out/fx/models/affect/sub-TPOT049/con_0022.nii'
+'/projects/adapt_lab/shared/TPOT/out/fx/models/affect/sub-TPOT051/con_0022.nii'
+'/projects/adapt_lab/shared/TPOT/out/fx/models/affect/sub-TPOT052/con_0022.nii'
+'/projects/adapt_lab/shared/TPOT/out/fx/models/affect/sub-TPOT053/con_0022.nii'
+'/projects/adapt_lab/shared/TPOT/out/fx/models/affect/sub-TPOT054/con_0022.nii'
+'/projects/adapt_lab/shared/TPOT/out/fx/models/affect/sub-TPOT055/con_0022.nii'
+'/projects/adapt_lab/shared/TPOT/out/fx/models/affect/sub-TPOT058/con_0022.nii'
+'/projects/adapt_lab/shared/TPOT/out/fx/models/affect/sub-TPOT060/con_0022.nii'
+'/projects/adapt_lab/shared/TPOT/out/fx/models/affect/sub-TPOT062/con_0022.nii'
+'/projects/adapt_lab/shared/TPOT/out/fx/models/affect/sub-TPOT064/con_0022.nii'
+'/projects/adapt_lab/shared/TPOT/out/fx/models/affect/sub-TPOT067/con_0022.nii'
+'/projects/adapt_lab/shared/TPOT/out/fx/models/affect/sub-TPOT068/con_0022.nii'
+'/projects/adapt_lab/shared/TPOT/out/fx/models/affect/sub-TPOT070/con_0022.nii'
+'/projects/adapt_lab/shared/TPOT/out/fx/models/affect/sub-TPOT071/con_0022.nii'
+'/projects/adapt_lab/shared/TPOT/out/fx/models/affect/sub-TPOT072/con_0022.nii'
+'/projects/adapt_lab/shared/TPOT/out/fx/models/affect/sub-TPOT074/con_0022.nii'
+'/projects/adapt_lab/shared/TPOT/out/fx/models/affect/sub-TPOT075/con_0022.nii'
+'/projects/adapt_lab/shared/TPOT/out/fx/models/affect/sub-TPOT079/con_0022.nii'
+'/projects/adapt_lab/shared/TPOT/out/fx/models/affect/sub-TPOT084/con_0022.nii'
+'/projects/adapt_lab/shared/TPOT/out/fx/models/affect/sub-TPOT086/con_0022.nii'
+'/projects/adapt_lab/shared/TPOT/out/fx/models/affect/sub-TPOT090/con_0022.nii'
+'/projects/adapt_lab/shared/TPOT/out/fx/models/affect/sub-TPOT091/con_0022.nii'
+'/projects/adapt_lab/shared/TPOT/out/fx/models/affect/sub-TPOT092/con_0022.nii'
+'/projects/adapt_lab/shared/TPOT/out/fx/models/affect/sub-TPOT093/con_0022.nii'
+'/projects/adapt_lab/shared/TPOT/out/fx/models/affect/sub-TPOT094/con_0022.nii'
+'/projects/adapt_lab/shared/TPOT/out/fx/models/affect/sub-TPOT097/con_0022.nii'
+'/projects/adapt_lab/shared/TPOT/out/fx/models/affect/sub-TPOT099/con_0022.nii'
+'/projects/adapt_lab/shared/TPOT/out/fx/models/affect/sub-TPOT101/con_0022.nii'
+'/projects/adapt_lab/shared/TPOT/out/fx/models/affect/sub-TPOT103/con_0022.nii'
+'/projects/adapt_lab/shared/TPOT/out/fx/models/affect/sub-TPOT106/con_0022.nii'
+'/projects/adapt_lab/shared/TPOT/out/fx/models/affect/sub-TPOT108/con_0022.nii'
+'/projects/adapt_lab/shared/TPOT/out/fx/models/affect/sub-TPOT112/con_0022.nii'
+'/projects/adapt_lab/shared/TPOT/out/fx/models/affect/sub-TPOT115/con_0022.nii'
+'/projects/adapt_lab/shared/TPOT/out/fx/models/affect/sub-TPOT116/con_0022.nii'
+'/projects/adapt_lab/shared/TPOT/out/fx/models/affect/sub-TPOT118/con_0022.nii'
+'/projects/adapt_lab/shared/TPOT/out/fx/models/affect/sub-TPOT120/con_0022.nii'
+'/projects/adapt_lab/shared/TPOT/out/fx/models/affect/sub-TPOT166/con_0022.nii'
+                                                          };
+%%
+matlabbatch{1}.spm.stats.factorial_design.des.t2.scans2 = {
+'/projects/adapt_lab/shared/TPOT/out/fx/models/affect/sub-TPOT005/con_0022.nii'
+'/projects/adapt_lab/shared/TPOT/out/fx/models/affect/sub-TPOT008/con_0022.nii'
+'/projects/adapt_lab/shared/TPOT/out/fx/models/affect/sub-TPOT020/con_0022.nii'
+'/projects/adapt_lab/shared/TPOT/out/fx/models/affect/sub-TPOT035/con_0022.nii'
+'/projects/adapt_lab/shared/TPOT/out/fx/models/affect/sub-TPOT056/con_0022.nii'
+'/projects/adapt_lab/shared/TPOT/out/fx/models/affect/sub-TPOT057/con_0022.nii'
+'/projects/adapt_lab/shared/TPOT/out/fx/models/affect/sub-TPOT059/con_0022.nii'
+'/projects/adapt_lab/shared/TPOT/out/fx/models/affect/sub-TPOT077/con_0022.nii'
+'/projects/adapt_lab/shared/TPOT/out/fx/models/affect/sub-TPOT078/con_0022.nii'
+'/projects/adapt_lab/shared/TPOT/out/fx/models/affect/sub-TPOT080/con_0022.nii'
+'/projects/adapt_lab/shared/TPOT/out/fx/models/affect/sub-TPOT081/con_0022.nii'
+'/projects/adapt_lab/shared/TPOT/out/fx/models/affect/sub-TPOT083/con_0022.nii'
+'/projects/adapt_lab/shared/TPOT/out/fx/models/affect/sub-TPOT085/con_0022.nii'
+'/projects/adapt_lab/shared/TPOT/out/fx/models/affect/sub-TPOT095/con_0022.nii'
+'/projects/adapt_lab/shared/TPOT/out/fx/models/affect/sub-TPOT096/con_0022.nii'
+'/projects/adapt_lab/shared/TPOT/out/fx/models/affect/sub-TPOT102/con_0022.nii'
+'/projects/adapt_lab/shared/TPOT/out/fx/models/affect/sub-TPOT104/con_0022.nii'
+'/projects/adapt_lab/shared/TPOT/out/fx/models/affect/sub-TPOT105/con_0022.nii'
+'/projects/adapt_lab/shared/TPOT/out/fx/models/affect/sub-TPOT107/con_0022.nii'
+'/projects/adapt_lab/shared/TPOT/out/fx/models/affect/sub-TPOT109/con_0022.nii'
+'/projects/adapt_lab/shared/TPOT/out/fx/models/affect/sub-TPOT110/con_0022.nii'
+'/projects/adapt_lab/shared/TPOT/out/fx/models/affect/sub-TPOT113/con_0022.nii'
+'/projects/adapt_lab/shared/TPOT/out/fx/models/affect/sub-TPOT114/con_0022.nii'
+'/projects/adapt_lab/shared/TPOT/out/fx/models/affect/sub-TPOT124/con_0022.nii'
+'/projects/adapt_lab/shared/TPOT/out/fx/models/affect/sub-TPOT125/con_0022.nii'
+'/projects/adapt_lab/shared/TPOT/out/fx/models/affect/sub-TPOT126/con_0022.nii'
+'/projects/adapt_lab/shared/TPOT/out/fx/models/affect/sub-TPOT127/con_0022.nii'
+'/projects/adapt_lab/shared/TPOT/out/fx/models/affect/sub-TPOT129/con_0022.nii'
+'/projects/adapt_lab/shared/TPOT/out/fx/models/affect/sub-TPOT130/con_0022.nii'
+'/projects/adapt_lab/shared/TPOT/out/fx/models/affect/sub-TPOT133/con_0022.nii'
+'/projects/adapt_lab/shared/TPOT/out/fx/models/affect/sub-TPOT134/con_0022.nii'
+'/projects/adapt_lab/shared/TPOT/out/fx/models/affect/sub-TPOT135/con_0022.nii'
+'/projects/adapt_lab/shared/TPOT/out/fx/models/affect/sub-TPOT137/con_0022.nii'
+'/projects/adapt_lab/shared/TPOT/out/fx/models/affect/sub-TPOT138/con_0022.nii'
+'/projects/adapt_lab/shared/TPOT/out/fx/models/affect/sub-TPOT139/con_0022.nii'
+'/projects/adapt_lab/shared/TPOT/out/fx/models/affect/sub-TPOT140/con_0022.nii'
+'/projects/adapt_lab/shared/TPOT/out/fx/models/affect/sub-TPOT142/con_0022.nii'
+'/projects/adapt_lab/shared/TPOT/out/fx/models/affect/sub-TPOT143/con_0022.nii'
+'/projects/adapt_lab/shared/TPOT/out/fx/models/affect/sub-TPOT146/con_0022.nii'
+'/projects/adapt_lab/shared/TPOT/out/fx/models/affect/sub-TPOT148/con_0022.nii'
+'/projects/adapt_lab/shared/TPOT/out/fx/models/affect/sub-TPOT149/con_0022.nii'
+'/projects/adapt_lab/shared/TPOT/out/fx/models/affect/sub-TPOT151/con_0022.nii'
+'/projects/adapt_lab/shared/TPOT/out/fx/models/affect/sub-TPOT152/con_0022.nii'
+'/projects/adapt_lab/shared/TPOT/out/fx/models/affect/sub-TPOT153/con_0022.nii'
+'/projects/adapt_lab/shared/TPOT/out/fx/models/affect/sub-TPOT154/con_0022.nii'
+'/projects/adapt_lab/shared/TPOT/out/fx/models/affect/sub-TPOT156/con_0022.nii'
+'/projects/adapt_lab/shared/TPOT/out/fx/models/affect/sub-TPOT158/con_0022.nii'
+'/projects/adapt_lab/shared/TPOT/out/fx/models/affect/sub-TPOT159/con_0022.nii'
+'/projects/adapt_lab/shared/TPOT/out/fx/models/affect/sub-TPOT160/con_0022.nii'
+'/projects/adapt_lab/shared/TPOT/out/fx/models/affect/sub-TPOT161/con_0022.nii'
+'/projects/adapt_lab/shared/TPOT/out/fx/models/affect/sub-TPOT162/con_0022.nii'
+'/projects/adapt_lab/shared/TPOT/out/fx/models/affect/sub-TPOT163/con_0022.nii'
+'/projects/adapt_lab/shared/TPOT/out/fx/models/affect/sub-TPOT165/con_0022.nii'
+'/projects/adapt_lab/shared/TPOT/out/fx/models/affect/sub-TPOT167/con_0022.nii'
+'/projects/adapt_lab/shared/TPOT/out/fx/models/affect/sub-TPOT171/con_0022.nii'
+'/projects/adapt_lab/shared/TPOT/out/fx/models/affect/sub-TPOT172/con_0022.nii'
+'/projects/adapt_lab/shared/TPOT/out/fx/models/affect/sub-TPOT174/con_0022.nii'
+'/projects/adapt_lab/shared/TPOT/out/fx/models/affect/sub-TPOT176/con_0022.nii'
+'/projects/adapt_lab/shared/TPOT/out/fx/models/affect/sub-TPOT177/con_0022.nii'
+'/projects/adapt_lab/shared/TPOT/out/fx/models/affect/sub-TPOT179/con_0022.nii'
+'/projects/adapt_lab/shared/TPOT/out/fx/models/affect/sub-TPOT181/con_0022.nii'
+'/projects/adapt_lab/shared/TPOT/out/fx/models/affect/sub-TPOT182/con_0022.nii'
+};
+
+matlabbatch{1}.spm.stats.factorial_design.des.t2.dept = 0;
+matlabbatch{1}.spm.stats.factorial_design.des.t2.variance = 1;
+matlabbatch{1}.spm.stats.factorial_design.des.t2.gmsca = 0;
+matlabbatch{1}.spm.stats.factorial_design.des.t2.ancova = 0;
+matlabbatch{1}.spm.stats.factorial_design.cov = struct('c', {}, 'cname', {}, 'iCFI', {}, 'iCC', {});
+matlabbatch{1}.spm.stats.factorial_design.multi_cov = struct('files', {}, 'iCFI', {}, 'iCC', {});
+matlabbatch{1}.spm.stats.factorial_design.masking.tm.tm_none = 1;
+matlabbatch{1}.spm.stats.factorial_design.masking.im = 0;
+matlabbatch{1}.spm.stats.factorial_design.masking.em = {'/projects/adapt_lab/jlewis5/ERdissertation/spm12/canonical/MNI152_T1_1mm_brain.nii,1'};
+matlabbatch{1}.spm.stats.factorial_design.globalc.g_omit = 1;
+matlabbatch{1}.spm.stats.factorial_design.globalm.gmsca.gmsca_no = 1;
+matlabbatch{1}.spm.stats.factorial_design.globalm.glonorm = 1;
+matlabbatch{2}.spm.stats.fmri_est.spmmat(1) = cfg_dep('Factorial design specification: SPM.mat File', substruct('.','val', '{}',{1}, '.','val', '{}',{1}, '.','val', '{}',{1}), substruct('.','spmmat'));
+matlabbatch{2}.spm.stats.fmri_est.write_residuals = 0;
+matlabbatch{2}.spm.stats.fmri_est.method.Classical = 1;
+matlabbatch{3}.spm.stats.con.spmmat(1) = cfg_dep('Model estimation: SPM.mat File', substruct('.','val', '{}',{2}, '.','val', '{}',{1}, '.','val', '{}',{1}), substruct('.','spmmat'));
+matlabbatch{3}.spm.stats.con.consess{1}.tcon.name = 'Group1_labelhappy > IB';
+matlabbatch{3}.spm.stats.con.consess{1}.tcon.weights = [-1 0];
+matlabbatch{3}.spm.stats.con.consess{1}.tcon.sessrep = 'none';
+matlabbatch{3}.spm.stats.con.consess{2}.tcon.name = 'Group2_labelhappy > IB';
+matlabbatch{3}.spm.stats.con.consess{2}.tcon.weights = [0 -1];
+matlabbatch{3}.spm.stats.con.consess{2}.tcon.sessrep = 'none';
+matlabbatch{3}.spm.stats.con.consess{3}.tcon.name = 'Group_1 > Group_2';
+matlabbatch{3}.spm.stats.con.consess{3}.tcon.weights = [-1 1];
+matlabbatch{3}.spm.stats.con.consess{3}.tcon.sessrep = 'none';
+matlabbatch{3}.spm.stats.con.consess{4}.tcon.name = 'Group_2 > Group_1';
+matlabbatch{3}.spm.stats.con.consess{4}.tcon.weights = [1 -1];
+matlabbatch{3}.spm.stats.con.consess{4}.tcon.sessrep = 'none';
+matlabbatch{3}.spm.stats.con.delete = 1;
